bbb7a017065d69d5b70a7487a126bf28021b052f
Aleksander Pasternak <aleksander.pasternak8@gmail.com>
2024-10-08 14:12:07 +0200

    Sekcja 4 skończona

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,11 @@
+###
+# Displaying student's personal data
+#
+# My name is Adam.
+# I am ... years old, and my height is ... cm.
+# In 6 years I will be ... years old. 
+#
+name = "Adam"
+age = 19
+height = 180
+print(f"My name is {name}. I am {age}  years old, and my height is {height} cm. In 6 years I will be {age+6} years old.")
