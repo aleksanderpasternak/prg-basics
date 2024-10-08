c77a488907607a022ee78c3e9a2f80604752b715
Aleksander Pasternak <aleksander.pasternak8@gmail.com>
2024-10-08 13:46:23 +0200

    Sekcja 3 skończona

---
+++ 02-TypesAndVariables/3-3.py
@@ -0,0 +1,14 @@
+###
+# A program for swapping two varable values
+#
+x = 7
+y = 34
+z = 7 - 34 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+
+# swap the values
+x = x - z
+y = y + z
+
+
+print("After swapping: x=", x, "y=", y)
---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,6 @@
+###
+# d = 3.57 × √h
+#
+height = input('Height of the observer: ')
+distance = 3.57 * int(height) ** 0.5
+print("Distance:", distance, "km")
